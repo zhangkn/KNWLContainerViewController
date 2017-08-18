b7488a1eafa7293268315f22fcc1feb35a71397c
zhangkunnan <zhangkunnan@qinbaowan.com>
2017-08-18 11:53:04 +0800

    Initial Commit

---
+++ KNWLContainerViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KNWLContainerViewController
+//
+//  Created by devzkn on 18/08/2017.
+//  Copyright © 2017 KN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
